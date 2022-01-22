252a4427370e698e1eba67a851d87194c475d036
bridget462 <bridges.dev.create@gmail.com>
2022-01-23 08:47:02 +0900

    refactor: white space adjusted for better readability

--- sudoku/sudoku_opt.py
+++ sudoku/sudoku_opt.py
@@ -1,6 +1,6 @@
+# global variables
 backtracks = 0 # to count how many backtrack call has been made
 EMPTY = 0 # empty cell value
-
 #x varies from entry1 to entry2 - 1, y varies from entry3 to entry4 - 1 
 sectors = [ [0, 3, 0, 3], [3, 6, 0, 3], [6, 9, 0, 3],
             [0, 3, 3, 6], [3, 6, 3, 6], [6, 9, 3, 6],
@@ -45,52 +45,42 @@ def is_cell_valid(grid, row, col, val):
 
 # IMPORTANT: This procedure makes implications based on existing numbers on squares
 def make_implications(grid, row, col, val):
-
     global sectors
 
     grid[row][col] = val
     impl = [(row, col, val)]
 
-
     for k in range(len(sectors)):
-
         sectinfo = []
-
         #find missing elements in ith sector
         vset = {1, 2, 3, 4, 5, 6, 7, 8, 9}
         for x in range(sectors[k][0], sectors[k][1]):
             for y in range(sectors[k][2], sectors[k][3]):
                 if grid[x][y] != EMPTY:
                     vset.remove(grid[x][y])
-
         #attach copy of vset to each missing square in ith sector
         for x in range(sectors[k][0], sectors[k][1]):
             for y in range(sectors[k][2], sectors[k][3]):
                 if grid[x][y] == EMPTY:
                     sectinfo.append([x, y, vset.copy()])
-        
         for m in range(len(sectinfo)):
             sin = sectinfo[m]
-            
             #find the set of elements on the row corresponding to m and remove them
             rowv = set()
             for y in range(9):
                 rowv.add(grid[sin[0]][y])
             left = sin[2].difference(rowv)
-            
             #find the set of elements on the column corresponding to m and remove them
             colv = set()
             for x in range(9):
                 colv.add(grid[x][sin[1]])
             left = left.difference(colv)
-                         
             #check if the vset is a singleton
             if len(left) == 1:
                 val = left.pop()
                 if is_cell_valid(grid, sin[0], sin[1], val):
                     grid[sin[0]][sin[1]] = val
                     impl.append((sin[0], sin[1], val))
-
     return impl
 
 
@@ -105,9 +95,7 @@ def undo_implications(grid, impl):
 #obeying the Sudoku rules by guessing when it has to and performing
 #implications when it can
 def solve_sudoku(grid, i = 0, j = 0):
-
-    global backtracks
-
+    global backtracks # for performance counting
     #find the next empty cell to fill
     i, j = find_first_empty_cell(grid)
     if i == -1:
@@ -116,15 +104,12 @@ def solve_sudoku(grid, i = 0, j = 0):
     for e in range(1, 10):
         #Try different values in i, j location
         if is_cell_valid(grid, i, j, e):
-
             impl = make_implications(grid, i, j, e)
-            
             if solve_sudoku(grid, i, j):
                 return True
             #Undo the current cell for backtracking
             backtracks += 1
             undo_implications(grid, impl)
-
     return False
 
 
@@ -138,6 +123,7 @@ def print_sudoku(grid):
     return
 
 
+# example input
 from input import example_girds
 
 solve_sudoku(example_girds["normal1"])
